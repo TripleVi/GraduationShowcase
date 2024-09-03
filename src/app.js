609c0e654904434ae3bae871327e0f7aef9cee47
Vuong Vu <vuongvu061101@gmail.com>
2024-09-04 00:13:33 +0700

    setup firebase cloud storage

--- src/app.js
+++ src/app.js
@@ -1,14 +1,24 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import 'dotenv/config'
+import { initializeApp, cert } from 'firebase-admin/app'
+import { getStorage } from 'firebase-admin/storage'
 
 import initRoutes from './routes'
+import serviceAccount from '../service-account-key.json'
 
 const app = express()
+
+initializeApp({
+  credential: cert(serviceAccount),
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+})
 
 app.use(bodyParser.json())
 
 initRoutes(app)
+
+const bucket = getStorage().bucket()
 
 const port = process.env.PORT || 3000
 app.listen(port, async () => {

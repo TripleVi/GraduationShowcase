d3cdcda836cabb30be7c4b2003817bcf0e94c1df
Vuong Vu <vuongvu061101@gmail.com>
2024-09-16 21:30:49 +0700

    update cors

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ import serviceAccount from '../service-account-key.json'
 const app = express()
 
 app.use(cors({
-  origin: '*',
+  origin: ['http://localhost:3000'],
   optionsSuccessStatus: 200,
   credentials: true,
 }))

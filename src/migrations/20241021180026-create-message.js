d0ca2da5f74d239fc952576c703cfa43e19015eb
Vuong Vu <vuongvu061101@gmail.com>
2024-10-25 14:43:25 +0700

    update Message model

--- src/migrations/20241021180026-create-message.js
+++ src/migrations/20241021180026-create-message.js
@@ -31,10 +31,6 @@ module.exports = {
       created_at: {
         allowNull: false,
         type: Sequelize.DATE
-      },
-      updated_at: {
-        allowNull: false,
-        type: Sequelize.DATE
       }
     });
   },
--- src/models/message.js
+++ src/models/message.js
@@ -25,6 +25,7 @@ module.exports = (sequelize, DataTypes) => {
       singular: 'message',
       plural: 'messages'
     },
+    updatedAt: false,
   });
   return Message;
 };
